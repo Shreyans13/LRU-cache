01529a4bc5e3e4da29abfd0653546d5a05402c1d
Shreyans Jain <shreyans.jain@juspay.in>
2025-04-27 20:38:40 +0530

    added linter

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,23 @@
 // const max8 = Math.pow(2, 8) - 1 
 // const max16 = Math.pow(2, 16) - 1 
 // const max32 = Math.pow(2, 32) - 1 
-const max8 = 255
-const max16 = 65535
-const max32 = 4294967295
 
-class LRUCache<K extends string | number | symbol, V extends string | number | symbol> {
+
+export class LRUCache<K extends string | number | symbol, V extends string | number | symbol> {
     private capacity: number
     private constants: {
-        MAX_INTEGER_8BIT: 255,
-        MAX_INTEGER_16BIT: 65535,
+        MAX_INTEGER_8BIT: 255
+        MAX_INTEGER_16BIT: 65535
         MAX_INTEGER_32BIT: 4294967295
     }
     private forward: Uint16Array<ArrayBuffer> | Uint8Array<ArrayBuffer> | Uint32Array<ArrayBuffer>
     private backward: Uint16Array<ArrayBuffer> | Uint8Array<ArrayBuffer> | Uint32Array<ArrayBuffer>
-    private KeyList: Array<K>;
-    private ValueList: Array<V>;
+    private KeyList: K[];
+    private ValueList: V[];
     private size: number
     private head: number
     private tail: number
-    private items: {
-        [key in K]: number
-    }
+    private items: Record<K, number>
 
     private getPointerArray = (size: number): Uint8ArrayConstructor | Uint16ArrayConstructor | Uint32ArrayConstructor | null => {
         const maxIndex = size - 1;
@@ -32,7 +28,7 @@ class LRUCache<K extends string | number | symbol, V extends string | number | s
                 ? Uint16Array : maxIndex <= this.constants.MAX_INTEGER_32BIT ? Uint32Array : null
     }
 
-    constructor(capacity: number, Keys, Values) {
+    constructor(capacity: number, Keys: new (size: number) => K[], Values: new (size: number) => V[]) {
         if (capacity <= 0)
             throw new Error("CAPACITY_SHOULD_BE_POSITIVE")
         if (!Number.isFinite(capacity) || Math.floor(capacity) !== capacity)
@@ -40,20 +36,25 @@ class LRUCache<K extends string | number | symbol, V extends string | number | s
         const PointerArray = this.getPointerArray(capacity)
         if (PointerArray == null)
             throw new Error("CAPACITY_NOT_SUPPORTED")
-
+        this.constants = {
+            MAX_INTEGER_8BIT: 255,
+            MAX_INTEGER_16BIT: 65535,
+            MAX_INTEGER_32BIT: 4294967295
+        }
         this.backward = new PointerArray()
         this.forward = new PointerArray()
 
-        this.KeyList = typeof Keys == 'function' ? new Keys(capacity) : new Array(capacity)
-        this.ValueList = typeof Keys == 'function' ? new Values(capacity) : new Array(capacity)
+        this.KeyList = typeof Keys == 'function' ? new Keys(capacity) : new Array<K>(capacity)
+        this.ValueList = typeof Keys == 'function' ? new Values(capacity) : new Array<V>(capacity)
 
         this.size = 0;
         this.head = 0;
         this.tail = 0;
-        this.items = {} as { [key in K]: V };;
+        this.capacity = capacity;
+        this.items = {} as Record<K, number>;
     }
 
-    private updatePointers = (pointer) => {
+    private updatePointers = (pointer: number) => {
         const oldHead = this.head;
         const previous = this.backward[pointer]
         const next = this.forward[pointer]
@@ -76,7 +77,7 @@ class LRUCache<K extends string | number | symbol, V extends string | number | s
         this.size = 0;
         this.head = 0;
         this.tail = 0;
-        this.items = {} as { [key in K]: V };;
+        this.items = {} as Record<K, number>;;
     }
 
     public set = (key: K, value: V) => {
@@ -91,7 +92,7 @@ class LRUCache<K extends string | number | symbol, V extends string | number | s
             else {
                 pointer = this.tail
                 this.tail = this.backward[pointer]
-                delete this.items[this.KeyList[pointer]]
+                delete this.items[this.KeyList[pointer]] // eslint-disable-line @typescript-eslint/no-dynamic-delete
             }
             this.items[key] = pointer;
             this.KeyList[pointer] = key;
